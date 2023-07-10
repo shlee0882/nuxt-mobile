d5508941e06471df6265b3c76cb9c6bb5a2fba37
SangHyun Lee <shlee0882@gmail.com>
2023-07-10 16:05:34 +0900

    tailwind css 설정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    '~/assets/main.css'
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
